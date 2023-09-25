0f21dfecb73779c9fb44267ec742d3a19f94cd03
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-09-25 19:56:20 +0500

    class#3 complete

---
+++ Class 3/index.js
@@ -0,0 +1,61 @@
+// Note* JS is run line by line and from left to right
+
+// UnFamilar Math Operator
+
+// increment and decrement
+
+// var num = 2;
+// num++ => num = num+1
+// num +=1
+// It increments the variable by 1.
+
+// var num = 1;
+// var newNum = num++;
+// what will be the value of 1
+
+// console.log(num); // 2
+// console.log(newNum); // 1
+
+// var a = 10;
+
+// console.log(a++); // 10
+// var result = a++ + --a - a-- + a++;
+// //   10 +  10 -  10 + 9
+
+// var a = 2;
+// var b = 3;
+
+// var result = --a + b++ - b-- + a-- + ++a - b--;
+
+// console.log(result); // -1
+
+// < ------------------------------------------- >
+
+// complex algebric equations
+
+// var exp = 2 * 3 * (4 + 3);
+// console.log(exp);
+
+// < ------------------------------------------- >
+
+// Concatenating text strings
+
+// let firstName = "Bilal";
+// let lastName = "Ahmed";
+
+// string to string
+// var fullName = firstName + " " + lastName;
+// console.log("Name: " + fullName);
+
+// string to number
+
+// var strToNum = "csc" + 112;
+
+// console.log("RollNo: " + strToNum);
+
+// number to string as a number
+// console.log(2 + 2 + "1" + 3);
+
+// -------------------------------------------- >
+// ----------------- Asignment ----------------
+// -------------------------------------------- >
