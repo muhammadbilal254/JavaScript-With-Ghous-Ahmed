ed98ba6f421f7654f74797b5b0097d40b296142a
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-09-25 21:43:30 +0500

    comparision operators

--- Class 4/index.js
+++ Class 4/index.js
@@ -44,27 +44,57 @@
 // }
 
 // Calculator
-document.write("<h1>Casio Calculator</h1>");
+// document.write("<h1>Casio Calculator</h1>");
 // convert string to number we use +operator
-var num1 = +prompt("Number 1");
-var oper = prompt("Enter Operator");
-var num2 = +prompt("Number 2");
-var result;
+// var num1 = +prompt("Number 1");
+// var oper = prompt("Enter Operator");
+// var num2 = +prompt("Number 2");
+// var result;
 
-if (oper == "+") {
-  result = num1 + num2;
-}
+// if (oper == "+") {
+//   result = num1 + num2;
+// }
 
-if (oper == "-") {
-  result = num1 - num2;
-}
+// if (oper == "-") {
+//   result = num1 - num2;
+// }
 
-if (oper == "*") {
-  result = num1 * num2;
-}
+// if (oper == "*") {
+//   result = num1 * num2;
+// }
 
-if (oper == "/") {
-  result = num1 / num2;
-}
+// if (oper == "/") {
+//   result = num1 / num2;
+// }
 
-alert(result);
+// alert(result);
+
+// Comparision Operator
+
+/*
+ *1. type and value check => ===
+ *2. value check => ==
+ *3. greater than => >
+ *4. greater than equal to => >=
+ *5. less than equal to => <
+ *6. less than equal to => <=
+ *7. not equal value => !=
+ *8. not equal value and type => !==
+ */
+
+// if ("23" === 23) {
+//   console.log(" equal");
+// }
+
+console.log(23 === "23"); // false
+console.log(23 == "23"); // true
+console.log(23 > 23); // false
+console.log(23 >= 23); // true
+console.log(24 > 12); // true
+console.log(23 < 23); // false
+console.log(23 <= 23); // true
+console.log(24 < 12); // false
+console.log(12 != 23); // true
+console.log(12 != 12); // false
+console.log(12 !== "12"); // true
+console.log(12 != "12"); // false
