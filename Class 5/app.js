d918f2bee81bde57c61ed2b7307f454bcd27f300
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-09-26 22:44:58 +0500

    add class 5

---
+++ Class 5/app.js
@@ -0,0 +1,10 @@
+// if else else-if statement
+
+var food = prompt("Kiya lao?");
+if (food === "biryani") {
+  alert("le ao");
+} else if (food === "burger") {
+  alert("2 pkr lao");
+} else {
+  alert("Ghar mt ao");
+}
